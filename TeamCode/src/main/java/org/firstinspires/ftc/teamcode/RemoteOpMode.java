db59f1fa42e6afc0ff69c484252f2d823426ac48
Tommy Poll <Tommy@poolle.co.uk>
2019-01-09 16:56:05 +0000

    Changed to 4-motor design
    removed arm code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RemoteOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RemoteOpMode.java
@@ -8,19 +8,25 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp
 public class RemoteOpMode extends LinearOpMode {
 
-    private DcMotor motorL;
-    private DcMotor motorR;
+    private DcMotor motorFL;
+    private DcMotor motorFR;
+    private DcMotor motorBL;
+    private DcMotor motorBR;
 
     @Override
     public void runOpMode(){
-        motorL = hardwareMap.get(DcMotor.class, "motor 0 HD hex");
-        motorR = hardwareMap.get(DcMotor.class, "motor 2 HD hex");
+        motorFL = hardwareMap.get(DcMotor.class, "motor 1 HD hex");
+        motorFR = hardwareMap.get(DcMotor.class, "motor 3 HD hex");
+        motorBL = hardwareMap.get(DcMotor.class, "motor 2 HD hex");
+        motorBR = hardwareMap.get(DcMotor.class, "motor 0 HD hex");
 
         telemetry.addData("Status", "Initialised");
         telemetry.update();
 
-        motorL.setDirection(DcMotor.Direction.FORWARD);
-        motorR.setDirection(DcMotor.Direction.REVERSE);
+        motorFL.setDirection(DcMotor.Direction.FORWARD);
+        motorFR.setDirection(DcMotor.Direction.REVERSE);
+        motorBL.setDirection(DcMotor.Direction.FORWARD);
+        motorBR.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
 
@@ -28,16 +34,18 @@ public class RemoteOpMode extends LinearOpMode {
         double powerR;
         while (opModeIsActive()) {
             double speed = -gamepad1.left_stick_y;
-            double direction =  gamepad1.right_stick_x;
+            double direction = 2*gamepad1.left_trigger - 1; //On the REV gamepad the right stick x is mapped as the left trigger
 
             powerL = Range.clip(speed + direction, -1.0, 1.0) ;
             powerR = Range.clip(speed - direction, -1.0, 1.0) ;
 
-            motorL.setPower(powerL);
-            motorR.setPower(powerR);
+            motorBL.setPower(powerL);
+            motorFL.setPower(powerL);
+            motorFR.setPower(powerR);
+            motorBR.setPower(powerR);
 
-            telemetry.addData("Left motor power", motorL.getPower());
-            telemetry.addData("Right motor power", motorR.getPower());
+            telemetry.addData("Left motor power", motorFL.getPower());
+            telemetry.addData("Right motor power", motorFR.getPower());
             telemetry.update();
         }
     }

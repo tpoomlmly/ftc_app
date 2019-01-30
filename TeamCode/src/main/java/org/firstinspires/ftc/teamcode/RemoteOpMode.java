864615f7b340672eda9826a7a3a4c3be919867af
Tommy Poll <Tommy@poolle.co.uk>
2019-01-30 11:08:32 +0000

    Initialise variables outside of main loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RemoteOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RemoteOpMode.java
@@ -19,15 +19,19 @@ public class RemoteOpMode extends LinearOpMode {
 
         double powerL;
         double powerR;
+        double speed;
+        double direction;
+        int armDirection;
+        int rotorDirection;
         while (opModeIsActive()) {
             telemetry.addData("Running time", h.runtime.time());
 
-            //double speed = -gamepad1.left_stick_y;
-            //double direction = 2*gamepad1.left_trigger - 1; //On the REV gamepad the right stick x is mapped as the left trigger
-            double speed = gamepad1.right_trigger - gamepad1.left_trigger;
-            double direction = gamepad1.right_stick_x;
-            int armDirection = (gamepad1.a ? 1 : 0) - (gamepad1.b ? 1 : 0);
-            int rotorDirection = (gamepad1.right_bumper ? 1 : 0) - (gamepad1.left_bumper ? 1 : 0);
+            speed = -gamepad1.left_stick_y;
+            direction = 2*gamepad1.left_trigger - 1; //On the REV gamepad the right stick x is mapped as the left trigger
+            //speed = gamepad1.right_trigger - gamepad1.left_trigger;
+            //direction = gamepad1.right_stick_x;
+            armDirection = (gamepad1.a ? 1 : 0) - (gamepad1.b ? 1 : 0);
+            rotorDirection = (gamepad1.right_bumper ? 1 : 0) - (gamepad1.left_bumper ? 1 : 0);
 
             powerL = Range.clip(speed + direction, -1.0, 1.0) ;
             powerR = Range.clip(speed - direction, -1.0, 1.0) ;

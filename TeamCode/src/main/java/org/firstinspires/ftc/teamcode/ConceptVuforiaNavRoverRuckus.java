22a99b96f8f5e4e9a352ac5ce6cf9d0289828cad
Tommy Poll <Tommy@poolle.co.uk>
2019-03-10 21:00:54 +0000

    Mild code cleaning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuforiaNavRoverRuckus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuforiaNavRoverRuckus.java
@@ -157,8 +157,7 @@ public class ConceptVuforiaNavRoverRuckus extends LinearOpMode {
         backSpace.setName("Back-Space");
 
         // For convenience, gather together all the trackable objects in one easily-iterable collection */
-        List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
-        allTrackables.addAll(targetsRoverRuckus);
+        List<VuforiaTrackable> allTrackables = new ArrayList<>(targetsRoverRuckus);
 
         /**
          * In order for localization to work, we need to tell the system where each target is on the field, and

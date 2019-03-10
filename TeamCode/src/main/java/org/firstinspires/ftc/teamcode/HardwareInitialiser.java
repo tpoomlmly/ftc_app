7195aacc3ddb4a2c9f26ff1d318b673ba8a00f30
Tommy Poll <Tommy@poolle.co.uk>
2019-03-10 21:01:30 +0000

    Update to latest hardware config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareInitialiser.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareInitialiser.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -9,35 +8,30 @@ class HardwareInitialiser
 {
     DcMotor leftMotor;
     DcMotor rightMotor;
-    DcMotor arm;
-    DcMotor hook;
-    DcMotorSimple grabber;
+    DcMotor rotor;
 
-    ElapsedTime runtime  = new ElapsedTime();
+    ElapsedTime runtime = new ElapsedTime();
 
     HardwareInitialiser(HardwareMap hwMap){
         this.leftMotor  = hwMap.get(DcMotor.class, "motor 0");
         this.rightMotor = hwMap.get(DcMotor.class, "motor 1");
-        this.arm        = hwMap.get(DcMotor.class, "motor 2");
-        //this.hook       = hwMap.get(DcMotor.class, "motor 3");
-        this.grabber    = hwMap.get(DcMotorSimple.class, "servo 0");
+        this.rotor      = hwMap.get(DcMotor.class, "motor 2");
+
+        this.leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        this.rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        this.rotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         this.leftMotor.setDirection(DcMotor.Direction.FORWARD);
         this.rightMotor.setDirection(DcMotor.Direction.REVERSE);
-        this.grabber.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        this.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        this.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        this.arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //this.hook.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        //this.hook.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        this.rotor.setDirection(DcMotor.Direction.FORWARD);
 
         this.leftMotor.setPower(0);
         this.rightMotor.setPower(0);
-        this.arm.setPower(0);
-        //this.hook.setPower(0);
-        this.grabber.setPower(0);
+        this.rotor.setPower(0);
+
+        this.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.rotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
- }
+}
 

d882ddda177d0ae9e569cf815bcf79e3ef419664
Tommy Poll <Tommy@poolle.co.uk>
2019-03-10 20:58:06 +0000

    Add blank autonomous phase opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousPhase.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class AutonomousPhase extends LinearOpMode {
+    @Override
+    public void runOpMode(){}
+}
